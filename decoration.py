58d0f8348d4d82e0eadef8aad8f8baee0c930804
evohnave <evohnave@users.noreply.github.com>
2020-01-10 09:39:45 -0500

    update f-string

--- decoration.py
+++ decoration.py
@@ -5,9 +5,9 @@ from time import time
 
 def timing(f):
     @wraps(f)
-    def wrap(*args, **kw):
+    def wrap(*args, **kwargs):
         ts = time()
-        result = f(*args, **kw)
+        result = f(*args, **kwargs)
         te = time()
         print(f'func: {f.__name__} args: {args}, kwargs: {kwargs} took: {ts-te:2.4f} sec')
         return result
